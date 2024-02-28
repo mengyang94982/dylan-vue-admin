678da749cd6530a15dd680f32a7ad09017432a2f
孟洋 <466879168@qq.com>
2024-02-28 23:11:57 +0800

    布局

---
+++ src/layouts/moodules/global-footer/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <a href="" target="_blank" rel="noopener noreferrer">
+    Copyright MIT © 2024 Dylan
+  </a>
+</template>
+
+<script
+  setup
+  lang="ts"
+>
+
+</script>
+
+<style scoped>
+
+</style>
