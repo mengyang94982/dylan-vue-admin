ae365334a6a2357809f1cb0053eca670008cba24
孟洋 <466879168@qq.com>
2024-03-06 23:42:08 +0800

    echarts

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,11 +2,24 @@
   <NSpace vertical :size="16">
     <HeaderBanner />
     <CardData />
+    <NGrid :x-gap='gap' :y-gap='16' responsive='screen' item-responsive>
+      <NGi span='24 s:24 m:14'>
+        <NCard :bordered='false' class='card-wrapper'>
+          <LineChart />
+        </NCard>
+      </NGi>
+      <NGi span='24 s:24 m:10'>
+        <NCard :bordered='false' class='card-wrapper'>
+          <PieChart />
+        </NCard>
+      </NGi>
+    </NGrid>
   </NSpace>
 </template>
 
 <script setup lang="ts">
   import HeaderBanner from "@/views/home/modules/header-banner.vue";
+  import CardData from '@/views/home/modules/card-data.vue'
 </script>
 
 
